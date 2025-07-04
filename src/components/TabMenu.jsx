914805ef71c08bf0947d681792aed3383c787fc7
1f10230039 <s1f102300391@iniad.org>
2025-07-04 13:47:17 +0900

    タブメニューの削除

--- src/components/TabMenu.jsx
+++ src/components/TabMenu.jsx
@@ -1,9 +1,1 @@
-import { Link, useLocation } from "react-router-dom";
-import "./tabmenu.css"
-export default function TabMenu {
-    const location = useLocation();
 
-    const tabs = [
-        {path: "/", label: "Home", icon:"home"}
-    ]
-}
