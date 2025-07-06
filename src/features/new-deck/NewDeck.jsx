f2190d9973053006064b78de0bd595b7e26b2bef
1f10230039 <s1f102300391@iniad.org>
2025-07-07 04:40:19 +0900

    デッキ編集画面の基礎作り

--- src/features/new-deck/NewDeck.jsx
+++ src/features/new-deck/NewDeck.jsx
@@ -5,9 +5,22 @@ export default function NewDeck () {
     return (
         <>
             <section className="new-deck">
-                <input type="text" placeholder="デッキ名を入力してください" />
-                <input type="text" placeholder="タグを入力してください" />
-                <NewCardBtn />
+                <section className="new-deck__input">
+                    <h2>デッキ名</h2>
+                    <input type="text" placeholder="デッキ名を入力してください" />
+                    <h2>タグ</h2>
+                    <input type="text" placeholder="タグを入力してください" />
+                </section>
+                <section className="new-deck__card">
+                    <NewCardBtn />
+                    <section className="new-deck__card__information">
+                        <p>カード一覧</p>
+                        <p>5枚</p>
+                    </section>
+                    <section className="new-deck__card__summary">
+                        ここにカード一覧が表示される
+                    </section>
+                </section>
             </section>
         </>
     );
