6b571c969f82288556e8da2f3ba2f1c90d2e2cab
1f10230039 <s1f102300391@iniad.org>
2025-07-08 10:10:52 +0900

    デッキ一覧のデータの読み込み機能を使い

--- src/features/decks/Decks.jsx
+++ src/features/decks/Decks.jsx
@@ -1,12 +1,26 @@
-import React from "react";
-import "./decks.css"
+import React, { useEffect, useState } from "react";
+import "./decks.css";
 import NewDeckBtn from "../../components/new-deck-btn/NewDeckBtn";
 
-export default function Decks  ()  {
+export default function Decks()  {
+  const [decks, setDecks] = useState([]);
+  useEffect (() => {
+    const storedDecks = JSON.parse(localStorage.getItem("decks")) || [];
+    setDecks(storedDecks);
+  },[]);
+
   return (
     <>
       <NewDeckBtn />
-      <section className="decks__summary"></section>
+      <section className="decks__summary">
+        {decks.length === 0 ? (
+          <p>まだデッキがありません</p>
+        ):(
+          <ul>
+            {decks.map}
+          </ul>
+        )} 
+      </section>
     </>
   );
 }
